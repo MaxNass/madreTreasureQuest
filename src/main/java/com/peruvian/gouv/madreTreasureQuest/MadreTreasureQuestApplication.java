ddc562d4403290b69dae07c93f983c6f04af5439
Nassim <nmezghiche@workit-software.com>
2020-12-01 09:09:34 +0100

    init project

---
+++ src/main/java/com/peruvian/gouv/madreTreasureQuest/MadreTreasureQuestApplication.java
@@ -0,0 +1,13 @@
+package com.peruvian.gouv.madreTreasureQuest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadreTreasureQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadreTreasureQuestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peruvian/gouv/madreTreasureQuest/MadreTreasureQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peruvian.gouv.madreTreasureQuest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadreTreasureQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
